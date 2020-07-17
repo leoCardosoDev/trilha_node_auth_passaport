24f76192c93401f7b389095a25307bf32577de23
Leonardo Cardoso <leocardosodev@gmail.com>
2020-07-16 22:07:06 -0300

    chore: add controller router

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+require('./src/index')(app)
+
 app.listen(9000, () => {
   console.log('Express has been started')
 })
